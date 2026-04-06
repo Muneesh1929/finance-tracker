5698de82ddec30bd647a5acdf43afc25fc7e6f15
Muneesh1929 <ermac0029@gmail.com>
2026-04-06 19:40:47 +0530

    Revert "Added filtering + fixed dashboard user data"
    This reverts commit 130a1f82f68606aa21774c833445c4e35aa1216d.

--- routes/recordRoutes.js
+++ routes/recordRoutes.js
@@ -23,7 +23,9 @@ router.get("/records/new", isLoggedIn, isAdmin, (req, res) => {
 
 router.get("/records/view", isLoggedIn, isAnalyst, async (req, res) => {
     try {
-        let filter = {};
+        let filter = {
+            createdBy: req.user._id
+        };
         if (req.query.type) {
             filter.type = req.query.type;
         }
